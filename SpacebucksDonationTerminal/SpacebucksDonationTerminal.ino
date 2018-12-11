7d6168b8a1d48de3b3ae805bb8c35c46183bd4bc
Jaimyn Mayer <hello@jaimyn.com.au>
2018-12-12 08:02:09 +1000

    Version 1 nearly done, just need to add http request for balance/donate

--- SpacebucksDonationTerminal/SpacebucksDonationTerminal.ino
+++ SpacebucksDonationTerminal/SpacebucksDonationTerminal.ino
@@ -16,6 +16,9 @@
 #include <PN532.h>                        // PN532 Library
 
 // config
+const String welcomeText = "Tap Card/Phone";
+const int initAmount = 500; // Initial amount (in cents)
+const int buttonClickTimeout = 10000; // Milliseconds before everything resets
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
 const int I2C_SDA = D4;
@@ -32,12 +35,20 @@ SoftwareSerial SWSerial(PN_RX, PN_TX); // RX, TX
 PN532_SWHSU pn532swhsu(SWSerial);
 PN532 nfc(pn532swhsu);
 
+// Variables
+int donateAmount = initAmount;
+unsigned long lastButtonPress = 0;
+int currentBalance = 0;
+String currentRfidUID = "";
+bool confirmAmount = false;
+bool updateAmount = true;
+
 void setup()
 {
   pinMode(BTN_PLUS, INPUT);
   pinMode(BTN_OK, INPUT);
   pinMode(BTN_MINUS, INPUT );
-  
+
   Serial.begin(9600);
   Serial.println("Beginning Setup");
 
@@ -69,7 +80,7 @@ void setup()
 
   WiFi.mode(WIFI_STA);
   // Add your WiFi network(s) below
-  wifiMulti.addAP("HSBNEWiFi", "<>");
+  wifiMulti.addAP("HSBNEWiFi", "HSBNEPortHack");
   //wifiMulti.addAP("ssid_from_AP_2", "your_password_for_AP_2");
   while (wifiMulti.run() != WL_CONNECTED) {
     Serial.print(".");
@@ -125,61 +136,185 @@ String printPN532Response(uint8_t *response, uint8_t responseLength) {
 }
 
 void loop() {
-  lcd.clear();
-  lcd.write("Tap Card/Phone");
-  lcd.blink();
-
-  Serial.print("BTN Minus: ");
-  Serial.println(digitalRead(BTN_MINUS));
-  Serial.print("BTN Ok: ");
-  Serial.println(digitalRead(BTN_OK));
-  Serial.print("BTN Plus: ");
-  Serial.println(digitalRead(BTN_PLUS));
-
-  bool success;
-  uint8_t responseLength = 32;
-  uint8_t response[responseLength];
-
-  if (nfc.inListPassiveTarget()) {
-    Serial.println("Found something!");
-    uint8_t selectApdu[] = { 0x00, /* CLA */
-                             0xA4, /* INS */
-                             0x04, /* P1  */
-                             0x00, /* P2  */
-                             0x07, /* Length of AID  */
-                             0xF0, 0x00, 0x00, 0x00, 0x00, 0x42, 0x42, /* AID defined on Android App */
-                             0x00  /* Le  */
-                           };
-
-    if (nfc.inDataExchange(selectApdu, sizeof(selectApdu), response, &responseLength)) {
-      Serial.print("responseLength: "); Serial.println(responseLength);
+  if (confirmAmount) {
+    // Update the screen if necessary
+    if(updateAmount) {
       lcd.clear();
-      lcd.write("Found Card:     ");
-      lcd.write(printPN532Response(response, responseLength).c_str());
-      delay(3000);
-    } else {
-      Serial.println("Failed sending SELECT AID - trying card read");
-      
-      boolean success;
-      uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
-      uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+      String line = "     $" + String(float(donateAmount)/100, 2);
+      lcd.write(line.c_str());
+      lcd.setCursor(0, 2);
+      lcd.write("Confirm Amount?");
+      lcd.blink();
+      updateAmount = false;
+    }
 
-      // Wait for an ISO14443A type cards (Mifare, etc.).  When one is found
-      // 'uid' will be populated with the UID, and uidLength will indicate
-      // if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
-      success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+    if (lastButtonPress + buttonClickTimeout < millis()) {
+      lcd.clear();
+      lcd.write("Timed Out...");
+      lcd.noBlink();
+      donateAmount = initAmount;
+      lastButtonPress = millis();
+      currentBalance = 0;
+      currentRfidUID = "";
+      confirmAmount = false;
+      updateAmount = false;
+      delay(1500);
+    }
 
-      if (success) {
-        Serial.println("Found a card!");
-        Serial.print("UID Length: ");
-        Serial.print(uidLength, DEC);
-        Serial.println(" bytes");
-        Serial.print("UID Value: ");
+    // Handle the "ok" button click
+    if(!digitalRead(BTN_OK)) {
+      delay(20);
+      if(!digitalRead(BTN_OK)) {
+        processOkClick();
+      }
+    }
+
+    // Handle our "+" button click
+    if(!digitalRead(BTN_PLUS)) {
+      delay(20);
+      if(!digitalRead(BTN_PLUS)) {
+        delay(200);
+        updateAmount = true;
+        lastButtonPress = millis();
+        if (donateAmount >= 300) {
+          donateAmount = donateAmount + 100;
+        } else if (donateAmount >= 100 && donateAmount <300) {
+          donateAmount = donateAmount + 50;
+        } else {
+          donateAmount = donateAmount + 25;
+        }
+      }
+    }
+
+    // Handle our "-" button click
+    if(!digitalRead(BTN_MINUS)) {
+      delay(20);
+      if(!digitalRead(BTN_MINUS)) {
+        delay(200);
+        updateAmount = true;
+        lastButtonPress = millis();
+        if (donateAmount <= 25) {
+        }
+        else if (donateAmount > 300) {
+          donateAmount = donateAmount - 100;
+        } else if (donateAmount > 100 && donateAmount <=300) {
+          donateAmount = donateAmount - 50;
+        } else {
+          donateAmount = donateAmount - 25;
+        }
+      }
+    }
+    
+  } else {
+    lcd.clear();
+    lcd.write(welcomeText.c_str());
+    lcd.blink();
+
+    bool success;
+    uint8_t responseLength = 32;
+    uint8_t response[responseLength];
+
+    if (nfc.inListPassiveTarget()) {
+      Serial.println("Found something!");
+      uint8_t selectApdu[] = { 0x00, /* CLA */
+                               0xA4, /* INS */
+                               0x04, /* P1  */
+                               0x00, /* P2  */
+                               0x07, /* Length of AID  */
+                               0xF0, 0x00, 0x00, 0x00, 0x00, 0x42, 0x42, /* AID must match Android App */
+                               0x00  /* Le  */
+                             };
+
+      // Attempt to connect to a phone by broadcasting an AID
+      if (nfc.inDataExchange(selectApdu, sizeof(selectApdu), response, &responseLength)) {
+        yield();
+        Serial.print("responseLength: ");
+        Serial.println(responseLength);
         lcd.clear();
-        lcd.write("Found 13mhz card");
-        lcd.write(printPN532Response(uid, uidLength).c_str());
-        delay(2000);
+        lcd.write("Found Card:     ");
+        // We found a card so lets get the UUID, log it, and process the swipe
+        String rfidUID = printPN532Response(response, responseLength);
+        lcd.write(rfidUID.c_str());
+        processSwipe(rfidUID);
+        
+      } else { // if the above fails it's probably a card
+        Serial.println("Failed sending SELECT AID - trying card read");
+
+        boolean success;
+        uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
+        uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
+
+        success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, &uid[0], &uidLength);
+        yield();
+        
+        if (success) {
+          Serial.println("Found a card!");
+          Serial.print("UID Length: ");
+          Serial.print(uidLength, DEC);
+          Serial.println(" bytes");
+          Serial.print("UID Value: ");
+          lcd.clear();
+          lcd.write("Found 13mhz card");
+          
+          // We found a card so lets get the UUID and process the swipe
+          String rfidUID = printPN532Response(uid, uidLength);
+          lcd.write(rfidUID.c_str());
+          processSwipe(rfidUID);
+        }
       }
     }
   }
+}
+
+bool processSwipe(String rfidUID) {
+  currentRfidUID = rfidUID;
+  lastButtonPress = millis();
+  lcd.clear();
+  lcd.write("Please wait");
+  String balance = getBalance(rfidUID);
+  lcd.clear();
+  lcd.noBlink();
+  lcd.write("Current Balance:     $");
+  lcd.write(balance.c_str());
+  delay(2000);
+  confirmAmount = true;
+  updateAmount = true;
+}
+
+void processOkClick() {
+  confirmAmount = false;
+  if (donateMoney(currentRfidUID, donateAmount)) {
+    lcd.noBlink();
+    lcd.clear();
+    String line = "Donated $" + String(float(donateAmount)/100, 2);
+    lcd.write(line.c_str());
+    lcd.setCursor(0, 2);
+    lcd.write("succesfully!");
+    delay(3000);
+  } else {
+    lcd.noBlink();
+    lcd.clear();
+    lcd.write("Failed to donate   :(");
+    delay(1000);
+  }
+  currentRfidUID = "";
+  donateAmount = initAmount;
+  confirmAmount = false;
+}
+
+int donateMoney(String rfidUID, int amount) {
+  // Donates the specified amount (in cents) to the specified member.
+  // TODO: Actually implement the web request.
+  delay(500);
+  return true;
+}
+
+String getBalance(String rfidUID) {
+  // Returns the balance of a member (in cents) from their RFID UID.
+  // TODO: Actually implement the web request.
+  delay(500);
+  int balance = 350;
+  String bal = String(float(balance) / 100, 2);
+  Serial.println(bal);
+  return bal;
 }
